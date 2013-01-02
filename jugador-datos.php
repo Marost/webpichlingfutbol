1db3f69569b95faa5afbc29f2790fda6428025dd
Marco Lopez <mlopez@virtualsip.net>
2013-01-02 15:43:14 -0500

    css y seleccion de opciones en jugador

---
+++ jugador-datos.php
@@ -0,0 +1,24 @@
+<div class="posicion">
+    <img src="imagenes/jugador-cancha.jpg" width="600" height="430">
+</div>                                
+
+<div class="galeria">
+
+    <div class="slider noShadow">
+        <a rel="prettyPhoto" href="imagenes/upload/galeria1.jpg">
+            <img class="peKbGallery" data-delay="5" src="imagenes/upload/galeria1.jpg" /></a>
+    </div>
+            
+            
+    <div class="slider noShadow">
+        <a rel="prettyPhoto" href="imagenes/upload/galeria2.jpg">
+            <img class="peKbGallery" data-delay="5" src="imagenes/upload/galeria2.jpg" /></a>
+    </div>
+            
+            
+    <div class="slider noShadow">
+        <a rel="prettyPhoto" href="imagenes/upload/galeria3.jpg">
+            <img class="peKbGallery" data-delay="5" src="imagenes/upload/galeria3.jpg" /></a>
+    </div>
+    
+</div>
