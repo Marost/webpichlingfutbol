49f5627c0367e84b5df8127e452e4c23c3b305ea
MARCO LOPEZ <mlopez@virtualsip.net>
2013-01-07 17:31:56 -0500

    agregando footer

---
+++ w-footer.php
@@ -0,0 +1,6 @@
+<div class="footer-container">
+    <footer class="wrapper">
+        <p>Cristobal de Peralta N° 110 - Oficina 1002 - Santiago de Surco</p>
+        <p>Telefono: 4370297 / 4376855</p>
+    </footer>
+</div>
