c6ab8b11021b0f351854d882afd0846e5dc13df1
MARCO LOPEZ <mlopez@virtualsip.net>
2013-01-12 12:06:01 -0500

    agregando plugin ckeditor

---
+++ panel@pichling/js/plugins/ckeditor/config.js
@@ -0,0 +1,13 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function(config) {
+   config.filebrowserBrowseUrl = '/panel@pichling/js/plugins/kcfinder/browse.php?type=files';
+   config.filebrowserImageBrowseUrl = '/panel@pichling/js/plugins/kcfinder/browse.php?type=images';
+   config.filebrowserFlashBrowseUrl = '/panel@pichling/js/plugins/kcfinder/browse.php?type=flash';
+   config.filebrowserUploadUrl = '/panel@pichling/js/plugins/kcfinder/upload.php?type=files';
+   config.filebrowserImageUploadUrl = '/panel@pichling/js/plugins/kcfinder/upload.php?type=images';
+   config.filebrowserFlashUploadUrl = '/panel@pichling/js/plugins/kcfinder/upload.php?type=flash';
+};
