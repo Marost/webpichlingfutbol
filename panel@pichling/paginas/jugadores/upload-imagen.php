678161f9c23a395ca1c03c64d1df525a52de2bff
MARCO LOPEZ <mlopez@virtualsip.net>
2013-01-11 21:28:40 -0500

    prueba de subida de archivos

--- panel@pichling/paginas/jugadores/upload-imagen.php
+++ panel@pichling/paginas/jugadores/upload-imagen.php
@@ -1,5 +1,11 @@
 <?php
 
-echo $_POST["uploader"];
+echo "uploader: ".$_POST["uploader"]." - ".$_POST["uploader_0_tmpname"]."<br>";
+
+echo "filelist: ".$_POST["filelist"]." - ".$_POST["filelist_0_tmpname"]."<br>";
+
+echo "FileUploaded: ".$_POST["FileUploaded"]." - ".$_POST["FileUploaded_0_tmpname"]."<br>";
+
+echo "uploadfiles: ".$_POST["uploadfiles"]." - ".$_POST["uploadfiles_0_tmpname"]."<br>";
 
 ?>
