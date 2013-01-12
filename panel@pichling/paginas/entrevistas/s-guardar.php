11771906f9a3fdd56665f6663e7d3f79024f7809
MARCO LOPEZ <mlopez@virtualsip.net>
2013-01-12 14:58:08 -0500

    guardar imagen

--- panel@pichling/paginas/entrevistas/s-guardar.php
+++ panel@pichling/paginas/entrevistas/s-guardar.php
@@ -8,12 +8,19 @@ require_once('../../js/plugins/thumbs/ThumbLib.inc.php');
 $nombre=$_POST["nombre"];
 $url=getUrlAmigable(eliminarTextoURL($nombre));
 $contenido=$_POST["contenido"];
-$imagen=$_POST["imagen"];
-$imagen_carpeta=$_POST["imagen_carpeta"];
 $pub_fecha=$_POST["pub_fecha"];
 $pub_hora=$_POST["pub_hora"];
 $fecha_publicacion=$pub_fecha." ".$pub_hora;
 $publicar=1;
+
+//IMAGEN
+if($_POST['uploader_0_tmpname']<>""){
+	$imagen=$_POST["uploader_0_tmpname"];
+	$imagen_carpeta=fechaCarpeta()."/";	
+}else{
+	$imagen="";
+	$imagen_carpeta="";
+}
 
 //INSERTANDO DATOS
 $rst_guardar=mysql_query("INSERT INTO ".$tabla_suf."_entrevista (url, titulo, contenido, imagen, imagen_carpeta, fecha_publicacion, publicar) VALUES('$url', '".htmlspecialchars($nombre)."', '$contenido', '$imagen', '$imagen_carpeta', '$fecha_publicacion', $publicar);",$conexion);
