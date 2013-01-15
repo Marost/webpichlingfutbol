0b723774747fa78ce90f6f7cf5e6c53c13a7e7a2
MARCO LOPEZ <mlopez@virtualsip.net>
2013-01-15 00:14:03 -0500

    eliminacion de type="text/javascript"

--- nota.php
+++ nota.php
@@ -112,8 +112,8 @@
         </script>
 
         <!-- ADDTHIS -->
-        <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
-        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50dcdc400c54dc87"></script>
+        <script>var addthis_config = {"data_track_addressbar":true};</script>
+        <script src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50dcdc400c54dc87"></script>
 
     </head>
     <body>
