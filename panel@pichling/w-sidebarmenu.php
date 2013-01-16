e3b73dbcefa46221ce51097bea3d78900df2dc6b
MARCO LOPEZ <mlopez@virtualsip.net>
2013-01-16 18:32:37 -0500

    agregando nuevas opciones al menu del adminitrador

--- panel@pichling/w-sidebarmenu.php
+++ panel@pichling/w-sidebarmenu.php
@@ -15,17 +15,15 @@
                     <span>Opciones</span>
             </a>
             <ul>
-                <li><a href="<?php echo $url_admin; ?>paginas/entrevistas/lista.php" title="">
-                    <span class="icol-frames"></span>Entrevistas</a></li>
-
-                <li><a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php" title="">
-                    <span class="icol-frames"></span>Jugadores</a></li>
-
-                <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title="">
-                    <span class="icol-frames"></span>Noticias</a></li>
-                    
-                <li><a href="<?php echo $url_admin; ?>paginas/posiciones/lista.php" title="">
-                    <span class="icol-frames"></span>Posiciones</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/empresa/lista.php" title=""><span class="icos-frames"></span>Empresa</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/entrevistas/lista.php" title=""><span class="icos-frames"></span>Entrevistas</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/galeria/lista.php" title=""><span class="icos-frames"></span>Galería de Fotos</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php" title=""><span class="icos-frames"></span>Jugadores</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/nosotros/lista.php" title=""><span class="icos-frames"></span>Nosotros</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/posiciones/lista.php" title="" ><span class="icos-frames"></span>Posiciones</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php" title="" ><span class="icos-frames"></span>Usuarios</a></li>
+                <li><a href="<?php echo $url_admin; ?>paginas/videos/lista.php" title="" ><span class="icos-frames"></span>Videos</a></li>
             </ul>
         </li>
     </ul>
--- panel@pichling/w-topline.php
+++ panel@pichling/w-topline.php
@@ -13,10 +13,15 @@
             
             <li><a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php" title="" class="exp" id="current">Opciones</a>
                 <ul>
+                    <li><a href="<?php echo $url_admin; ?>paginas/empresa/lista.php">Empresa</a></li>
                     <li><a href="<?php echo $url_admin; ?>paginas/entrevistas/lista.php">Entrevistas</a></li>
+                    <li><a href="<?php echo $url_admin; ?>paginas/galeria/lista.php">Galería de Fotos</a></li>
                     <li><a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php">Jugadores</a></li>
+                    <li><a href="<?php echo $url_admin; ?>paginas/nosotros/lista.php">Nosotros</a></li>
                     <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php">Noticias</a></li>
                     <li><a href="<?php echo $url_admin; ?>paginas/posiciones/lista.php">Posiciones</a></li>
+                    <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php">Usuarios</a></li>
+                    <li><a href="<?php echo $url_admin; ?>paginas/videos/lista.php">Videos</a></li>
                 </ul>
             </li>
         </ul>
