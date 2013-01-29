bfff3b8b8414a1fec905f198b530f00245de1cec
Marco Lopez <mlopez@virtualsip.net>
2013-01-29 17:51:50 -0500

    enlace de eliminar a lista de galeria de jugadores

--- panel@pichling/paginas/jugadores/galeria/s-eliminar.php
+++ panel@pichling/paginas/jugadores/galeria/s-eliminar.php
@@ -6,16 +6,17 @@ require_once('../../../js/plugins/thumbs/ThumbLib.inc.php');
 
 //DECLARACION DE VARIABLES
 $id=$_REQUEST["id"];
+$jugador_id=$_REQUEST["jugador"];
 
 mysql_query("DELETE FROM ".$tabla_suf."_jugadores_galeria WHERE id=$id;", $conexion);
 
 if (mysql_errno()!=0){
 	echo "ERROR: <strong>".mysql_errno()."</strong> - ". mysql_error();
 	mysql_close($conexion);
-	header("Location:lista.php?msj=er");
+	header("Location:lista.php?jugador=$jugador_id&msj=er");
 } else {
 	mysql_close($conexion);
-	header("Location:lista.php?msj=el");
+	header("Location:lista.php?jugador=$jugador_id&msj=el");
 }
 
 ?>
