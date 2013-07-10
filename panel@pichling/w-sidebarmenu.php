0fd09e99440806b00c958cf1dabcbf464ecfad67
Marco Lopez <mlopez@virtualsip.net>
2013-07-10 12:47:42 -0500

    modificacion de lista de menu

--- panel@pichling/w-sidebarmenu.php
+++ panel@pichling/w-sidebarmenu.php
@@ -1,30 +1,17 @@
 <div class="mainNav">
     <div class="user">
         <a title="" class="leftUserDrop">
-            <img src="<?php echo $url_admin; ?>images/user.png" alt="" /></a><span>Eugene</span>
+            <img src="<?php echo $url_admin; ?>images/user.png" alt="" /></a><span></span>
     </div>
     
     <!-- Main nav -->
-    <ul class="nav">
-        <li><a href="<?php echo $url_admin; ?>index.php" title="">
-            <img src="<?php echo $url_admin; ?>images/icons/mainnav/dashboard.png" alt="" /><span>Dashboard</span></a></li>
-        
-        <li>
-            <a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php" title="">
-                <img src="<?php echo $url_admin; ?>images/icons/mainnav/tables.png" alt="" />
-                    <span>Opciones</span>
-            </a>
-            <ul>
-                <li><a href="<?php echo $url_admin; ?>paginas/empresa/lista.php" title=""><span class="icos-frames"></span>Empresa</a></li>
-                <li><a href="<?php echo $url_admin; ?>paginas/entrevistas/lista.php" title=""><span class="icos-frames"></span>Entrevistas</a></li>
-                <li><a href="<?php echo $url_admin; ?>paginas/galeria/lista.php" title=""><span class="icos-frames"></span>Galería de Fotos</a></li>
-                <li><a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php" title=""><span class="icos-frames"></span>Jugadores</a></li>
-                <li><a href="<?php echo $url_admin; ?>paginas/nosotros/lista.php" title=""><span class="icos-frames"></span>Nosotros</a></li>
-                <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
-                <li><a href="<?php echo $url_admin; ?>paginas/posiciones/lista.php" title="" ><span class="icos-frames"></span>Posiciones</a></li>
-                <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php" title="" ><span class="icos-frames"></span>Usuarios</a></li>
-                <li><a href="<?php echo $url_admin; ?>paginas/videos/lista.php" title="" ><span class="icos-frames"></span>Videos</a></li>
-            </ul>
-        </li>
+    <ul class="subNav">
+        <li><a href="<?php echo $url_admin; ?>paginas/entrevistas/lista.php" title=""><span class="icos-frames"></span>Entrevistas</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php" title=""><span class="icos-frames"></span>Jugadores</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/nosotros/lista.php" title=""><span class="icos-frames"></span>Nosotros</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/posiciones/lista.php" title="" ><span class="icos-frames"></span>Posiciones</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php" title="" ><span class="icos-frames"></span>Usuarios</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/videos/lista.php" title="" ><span class="icos-frames"></span>Videos</a></li>
     </ul>
 </div>
--- panel@pichling/w-topline.php
+++ panel@pichling/w-topline.php
@@ -1,29 +1,5 @@
 <div id="top">
     <div class="wrapper">
-        <a href="<?php echo $url_admin; ?>index.php" title="" class="logo"><img src="<?php echo $url_admin; ?>images/logo.png" alt="" /></a>
-        
-        <!-- Right top nav -->
-        <div class="topNav">
-            <a title="" class="iButton"></a>
-        </div>
-        
-        <!-- Responsive nav -->
-        <ul class="altMenu">
-            <li><a href="<?php echo $url_admin; ?>index.php" title="">Dashboard</a></li>
-            
-            <li><a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php" title="" class="exp" id="current">Opciones</a>
-                <ul>
-                    <li><a href="<?php echo $url_admin; ?>paginas/empresa/lista.php">Empresa</a></li>
-                    <li><a href="<?php echo $url_admin; ?>paginas/entrevistas/lista.php">Entrevistas</a></li>
-                    <li><a href="<?php echo $url_admin; ?>paginas/galeria/lista.php">Galería de Fotos</a></li>
-                    <li><a href="<?php echo $url_admin; ?>paginas/jugadores/lista.php">Jugadores</a></li>
-                    <li><a href="<?php echo $url_admin; ?>paginas/nosotros/lista.php">Nosotros</a></li>
-                    <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php">Noticias</a></li>
-                    <li><a href="<?php echo $url_admin; ?>paginas/posiciones/lista.php">Posiciones</a></li>
-                    <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php">Usuarios</a></li>
-                    <li><a href="<?php echo $url_admin; ?>paginas/videos/lista.php">Videos</a></li>
-                </ul>
-            </li>
-        </ul>
+        <a href="<?php echo $url_admin; ?>index.php" title="" class="logo"></a>
     </div>
 </div>
